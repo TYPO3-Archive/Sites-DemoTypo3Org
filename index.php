e94d2326b14a34b3add8ed8501c5668194f7a34f
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-04-25 02:37:54 +0200

    [TASK] Add redirection in index.php
    Change-Id: Id0fea0fa0eba5be3bc6748f7d625e1a99d85c24e

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
 
+# Redirect packages
+foreach (array('introduction', 'neos') as $distribution) {
+    if (strpos($_SERVER['REQUEST_URI'], '/' . $distribution) === 0) {
+        $segment = ltrim($_SERVER['REQUEST_URI'], '/');
+        $parts = explode('/', $segment);
+        $host = array_shift($parts);
+        $path = implode('/', $parts);
+        $path = ltrim($path, '/');
+
+        $location = sprintf('http://%s.cms.demo.typo3.org/%s', $host, $path);
+        if ($distribution == 'neos') {
+            $location = sprintf('http://%s.demo.typo3.org/%s', $host, $path);
+        }
+        header('Location: ' . $location);
+        die();
+    }
+}
+
+
 $template = 'LandingPage/template.html';
 $output = file_get_contents($template);
 
@@ -9,6 +28,4 @@ $content = file_get_contents($contentTemplate);
 $output = str_replace('###CONTENT###', $content, $output);
 $output = str_replace('../', '/LandingPage/', $output);
 
-echo $output;
-
-?>
+print $output;
